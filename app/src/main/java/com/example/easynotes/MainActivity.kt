421de80d94e628622e901162b35dff8c1cc09102
IshwarChand9312 <ishwarchand9312@gmail.com>
2020-10-14 22:23:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/easynotes/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.easynotes
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    public fun Register_btn(view : View){
+
+        startActivity(Intent(this@MainActivity,Register_activity::class.java))
+    }
+
+    public fun Login_btn(view : View){
+
+        startActivity(Intent(this@MainActivity,Login_activity::class.java))
+
+    }
+}
